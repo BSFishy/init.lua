4ddadf34b1fec3c2b9c7144d9ea0b1b87bf38a33
Matt Provost <provomat@amazon.com>
2023-11-20 18:26:18 +0000

    Add osc52
    Signed-off-by: Matt Provost <provomat@amazon.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- osc52
+-- TODO: delete this in nvim v0.10.0
+vim.keymap.set("n", "<leader>y", require("osc52").copy_operator, { expr = true })
+vim.keymap.set("n", "<leader>yy", "<leader>c_", { remap = true })
+vim.keymap.set("v", "<leader>y", require("osc52").copy_visual)
