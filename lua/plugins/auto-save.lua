5b3fa8b9717cf8337e0d7d5f149058582bed79b6
Matt Provost <provomat@amazon.com>
2023-11-21 21:02:35 +0000

    Update some of the auto save config
    Signed-off-by: Matt Provost <provomat@amazon.com>

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -1,5 +1,10 @@
 return {
   {
     "pocco81/auto-save.nvim",
+    opts = {
+      trigger_events = { "InsertLeave", "CursorHoldI", "FocusLost" },
+      write_all_buffers = true,
+      debounce_delay = 5000,
+    },
   },
 }
