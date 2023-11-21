9647f911f7b1e2c1ab7bf26e6198f84819f99d1a
Matt Provost <mattprovost6@gmail.com>
2023-11-21 14:37:53 -0800

    Add another gruvbox override
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -13,6 +13,7 @@ return {
       transparent_mode = true,
       overrides = {
         ["@variable"] = { link = "GruvboxBlue" },
+        ["@namespace"] = { link = "GruvboxOrange" },
       },
     },
   },
