86a1bef7d94304ce351a5686986544abf1edc7f4
Matt Provost <provomat@amazon.com>
2023-12-12 20:22:10 +0000

    Add <C-q> keybind
    Signed-off-by: Matt Provost <provomat@amazon.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,23 @@
 vim.keymap.set("n", "<leader>y", require("osc52").copy_operator, { expr = true })
 vim.keymap.set("n", "<leader>yy", "<leader>c_", { remap = true })
 vim.keymap.set("v", "<leader>y", require("osc52").copy_visual)
+
+-- Intellij-style info of symbol under cursor
+vim.keymap.set(
+  "n",
+  "<C-q>",
+  "<cmd>lua vim.lsp.buf.hover()<CR>",
+  { noremap = true, silent = true, desc = "Show symbol information" }
+)
+vim.keymap.set(
+  "i",
+  "<C-q>",
+  "<cmd>lua vim.lsp.buf.hover()<CR>",
+  { noremap = true, silent = true, desc = "Show symbol information" }
+)
+vim.keymap.set(
+  "v",
+  "<C-q>",
+  "<cmd>lua vim.lsp.buf.hover()<CR>",
+  { noremap = true, silent = true, desc = "Show symbol information" }
+)
