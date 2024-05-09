ee82518a79a9d306f9cf9bf1a3fb5bc4f1d22148
Matt Provost <mattprovost6@gmail.com>
2024-05-08 20:30:44 -0700

    Add Nix
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -4,6 +4,8 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "gopls",
+        "nil",
+        "nixpkgs-fmt",
         "svelte-language-server",
       })
     end,
