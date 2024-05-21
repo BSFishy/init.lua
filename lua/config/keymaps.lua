d58891ef907a290dc1209c198a7e0c8ef884dcca
Matt Provost <mattprovost6@gmail.com>
2024-05-21 09:56:39 -0700

    Revert osc52 keymap
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,12 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- osc52
--- TODO: delete this in nvim v0.10.0
-vim.keymap.set("n", "<leader>y", require("osc52").copy_operator, { expr = true })
-vim.keymap.set("n", "<leader>yy", "<leader>c_", { remap = true })
-vim.keymap.set("v", "<leader>y", require("osc52").copy_visual)
-
 -- Intellij-style info of symbol under cursor
 vim.keymap.set(
   "n",
