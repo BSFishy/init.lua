b016ecd0429b88b0d1baebdbb593d1317e2e2a88
Matt Provost <mprovost@cloudflare.com>
2024-09-30 11:33:38 -0500

    feat: add signature help keybind
    Signed-off-by: Matt Provost <mprovost@cloudflare.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,22 +4,28 @@
 
 local map = vim.keymap.set
 
+-- I don't really use this anymore
 -- Intellij-style info of symbol under cursor
-map(
-  "n",
-  "<C-q>",
-  "<cmd>lua vim.lsp.buf.hover()<CR>",
-  { noremap = true, silent = true, desc = "Show symbol information" }
-)
-map(
-  "i",
-  "<C-q>",
-  "<cmd>lua vim.lsp.buf.hover()<CR>",
-  { noremap = true, silent = true, desc = "Show symbol information" }
-)
-map(
-  "v",
-  "<C-q>",
-  "<cmd>lua vim.lsp.buf.hover()<CR>",
-  { noremap = true, silent = true, desc = "Show symbol information" }
-)
+-- map(
+--   "n",
+--   "<C-q>",
+--   "<cmd>lua vim.lsp.buf.hover()<CR>",
+--   { noremap = true, silent = true, desc = "Show symbol information" }
+-- )
+-- map(
+--   "i",
+--   "<C-q>",
+--   "<cmd>lua vim.lsp.buf.hover()<CR>",
+--   { noremap = true, silent = true, desc = "Show symbol information" }
+-- )
+-- map(
+--   "v",
+--   "<C-q>",
+--   "<cmd>lua vim.lsp.buf.hover()<CR>",
+--   { noremap = true, silent = true, desc = "Show symbol information" }
+-- )
+
+-- Signature help
+map("i", "<C-h>", function()
+  vim.lsp.buf.signature_help()
+end, { desc = "Signature help" })
