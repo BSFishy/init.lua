3494b63f3d4a89fc16122a1b98f179bfd271206e
Matt Provost <mattprovost6@gmail.com>
2025-01-15 19:57:08 -0600

    feat: idk sum stuff
    erm so i mainly want this for the auto text wrapping idk about the jinja stuff i
    dont think it really works that well
    
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,19 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.yml.j2",
+  callback = function()
+    vim.cmd("set filetype=jinja.yaml")
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.textwidth = 80 -- Set desired wrap width
+    vim.opt_local.formatoptions:append("c") -- Wrap comments
+    vim.opt_local.formatoptions:append("r") -- Continue comments on Enter
+  end,
+})
--- lua/plugins/jinja.lua
+++ lua/plugins/jinja.lua
@@ -1,5 +1,8 @@
 return {
   {
     "HiPhish/jinja.vim",
+    config = function()
+      vim.g["jinja#syntax_for_host"] = "yaml"
+    end,
   },
 }
