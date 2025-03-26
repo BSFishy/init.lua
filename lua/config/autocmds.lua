d85bddaf5eafc52fd3bcb2c127e0340eebfcbe06
Matt Provost <mprovost@cloudflare.com>
2025-03-26 11:09:16 -0500

    feat: properly support proto
    this was really annoying me for a bit so i just finally actually fixed it
    
    Signed-off-by: Matt Provost <mprovost@cloudflare.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,7 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- setup jinja template for yml.j2 files
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = "*.yml.j2",
   callback = function()
@@ -9,11 +10,29 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   end,
 })
 
+-- hard wrap comments at 80 column
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "*", "markdown" },
   callback = function()
     vim.opt_local.textwidth = 80 -- Set desired wrap width
     vim.opt_local.formatoptions:append("c") -- Wrap comments
     vim.opt_local.formatoptions:append("r") -- Continue comments on Enter
+  end,
+})
+
+-- start the protols lsp for proto files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "proto",
+  callback = function()
+    local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+    local has_protols = vim.tbl_contains(
+      vim.tbl_map(function(c)
+        return c.name
+      end, clients),
+      "protols"
+    )
+    if not has_protols then
+      vim.cmd("LspStart protols")
+    end
   end,
 })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -2,6 +2,16 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
+      servers = {
+        -- remove proto
+        clangd = {
+          filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+        },
+        -- configure protols
+        protols = {
+          filetypes = { "proto" },
+        },
+      },
       inlay_hints = {
         enabled = false,
       },
