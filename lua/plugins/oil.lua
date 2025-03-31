1c5471836f6a19cc1d63e065216706a639e1679b
Matt Provost <mprovost@cloudflare.com>
2025-03-31 09:53:27 -0500

    chore: oil keybinds
    now i have <leader>O for opening oil in the root directory, as well as <Esc> can
    close oil.
    
    Signed-off-by: Matt Provost <mprovost@cloudflare.com>

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,9 +6,19 @@ return {
       view_options = {
         show_hidden = true,
       },
+      keymaps = {
+        ["<Esc>"] = "actions.close",
+      },
     },
     keys = {
       { "<leader>o", "<cmd>Oil<cr>", desc = "Open parent directory in Oil" },
+      {
+        "<leader>O",
+        function()
+          require("oil").open(vim.fn.getcwd())
+        end,
+        desc = "Open Oil at project root",
+      },
     },
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
