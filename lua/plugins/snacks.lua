1f86941a38d1ff1ec5cbeb4be7526abe016ced8b
Matt Provost <mprovost@cloudflare.com>
2025-12-02 15:27:04 +0000

    chore: minor gitbrowse config
    Signed-off-by: Matt Provost <mprovost@cloudflare.com>

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,6 +3,17 @@ return {
     "snacks.nvim",
     opts = {
       scroll = { enabled = false },
+      gitbrowse = {
+        url_patterns = {
+          ["gitlab%.%w+%.?%w*"] = {
+            -- copy of `gitlab%.com` from default config
+            branch = "/-/tree/{branch}",
+            file = "/-/blob/{branch}/{file}#L{line_start}-L{line_end}",
+            permalink = "/-/blob/{commit}/{file}#L{line_start}-L{line_end}",
+            commit = "/-/commit/{commit}",
+          },
+        },
+      },
     },
   },
 }
