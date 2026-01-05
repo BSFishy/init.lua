df45b63ed637bea3a8d20d964fa631fcdbfd6197
Matt Provost <mprovost@cloudflare.com>
2026-01-05 16:39:52 +0000

    chore: disable mouse
    Signed-off-by: Matt Provost <mprovost@cloudflare.com>

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Disable mouse
+vim.opt.mouse = ""
+
 -- Set ruby lsp to use solargraph
 vim.g.lazyvim_ruby_lsp = "solargraph"
 
