f5e8cc2c449c0d7a449729cbc4ad0c1208b0c719
Matt Provost <mattprovost6@gmail.com>
2026-02-16 22:38:55 +0000

    feat: add fzf autocomplete thing
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -321,6 +321,74 @@ return {
         end,
         desc = "Goto Symbol (Workspace)",
       },
+      {
+        "<c-f>",
+        function()
+          local params = {
+            textDocument = vim.lsp.util.make_text_document_params(),
+            position = vim.lsp.util.make_position_params(0, 'utf-8').position,
+            context = { triggerKind = 1 },
+          }
+
+          local clients_completed = 0
+          local completions = {}
+          local requests, cancel = vim.lsp.buf_request(0, "textDocument/completion", params, function(_, result)
+            if not result then
+              clients_completed = clients_completed + 1
+              return
+            end
+
+            local items = result.items or result
+            for _, item in ipairs(items) do
+              local display = string.format("%10s %s",
+                "[" .. (vim.lsp.protocol.CompletionItemKind[item.kind] or "?") .. "]",
+                item.label
+              )
+
+              completions[display] = item
+            end
+
+            clients_completed = clients_completed + 1
+          end)
+
+          local success = vim.wait(10000, function()
+            return clients_completed == #requests
+          end, 10)
+
+          if not success then
+            cancel()
+            vim.notify("Completions not found in time", vim.log.levels.ERROR)
+            return
+          end
+
+          FzfLua.fzf_exec(
+            vim.tbl_keys(completions),
+            {
+              actions = {
+                ['default'] = function(selected)
+                  assert(#selected == 1, "Invalid selection")
+
+                  local item = completions[selected[1]]
+                  local SNIPPET = vim.lsp.protocol.InsertTextFormat.Snippet
+                  local text = item.insertText or item.label
+
+                  if item.insertTextFormat == SNIPPET then
+                    vim.snippet.expand(text)
+                  else
+                    vim.api.nvim_put({ text }, "c", true, true)
+                  end
+
+                  if item.command then
+                    vim.lsp.buf_request(0, "workspace/executeCommand", { command = item.command })
+                  end
+                end,
+              },
+            }
+          )
+        end,
+        mode = "i",
+        desc = "LSP Search",
+      },
     },
   },
 
